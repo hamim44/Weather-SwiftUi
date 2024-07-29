665491d9c57ea692df206dfc571083dad62e5df0
Abrar hamim <abrarhamim4044@gmail.com>
2024-07-29 18:19:48 +0600

    Initial Commit

---
+++ Weather-swiftUi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Weather-swiftUi
+//
+//  Created by Abrar Hamim on 29/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Weather-swiftUi/Weather_swiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_swiftUiApp.swift
+//  Weather-swiftUi
+//
+//  Created by Abrar Hamim on 29/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Weather_swiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
